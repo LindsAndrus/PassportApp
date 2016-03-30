6cacb0302a7055144a7e69fdf4dcb83768517ff3
Ethan Choi <dchoi33@yahoo.com>
2016-03-30 14:35:21 -0700

    hourly push

--- client/static/javascript.js
+++ client/static/javascript.js
@@ -1,49 +1,48 @@
-function success(position) {
-  var mapcanvas = document.createElement('div');
-  mapcanvas.id = 'mapcontainer';
-  mapcanvas.style.height = '300px';
-  mapcanvas.style.width = '560px';
+// function success(position) {
+//   var mapcanvas = document.createElement('div');
+//   mapcanvas.id = 'mapcontainer';
+//   mapcanvas.style.height = '300px';
+//   mapcanvas.style.width = '560px';
+//
+//   document.querySelector('article').appendChild(mapcanvas);
+//
+//   var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//   var currentLat = position.coords.latitude;
+//   var currentLng =  position.coords.longitude;
+//   console.log(currentLat, currentLng);
+//
+//   var options = {
+//     zoom: 15,
+//     center: coords,
+//     mapTypeControl: false,
+//     navigationControlOptions: {
+//     	style: google.maps.NavigationControlStyle.SMALL
+//     },
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+//
+//   var marker = new google.maps.Marker({
+//       position: coords,
+//       map: map,
+//       title:"You are here!"
+//   });
+// }
+//
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(success);
+// } else {
+//   error('Geo Location is not supported');
+// }
 
-  document.querySelector('article').appendChild(mapcanvas);
-
-  var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  var currentLat = position.coords.latitude;
-  var currentLng =  position.coords.longitude;
-  console.log(currentLat, currentLng);
-
-  var options = {
-    zoom: 15,
-    center: coords,
-    mapTypeControl: false,
-    navigationControlOptions: {
-    	style: google.maps.NavigationControlStyle.SMALL
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
-
-  var marker = new google.maps.Marker({
-      position: coords,
-      map: map,
-      title:"You are here!"
-  });
-}
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success);
-} else {
-  error('Geo Location is not supported');
-}
-
-
-
-var map;
+var mapper;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  mapper = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 }
+
 // $(document).ready(function(){
 //   initMap();
 // })
