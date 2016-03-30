d299690e2471703ea82cd166f72587712a1d9212
LindsAndrus <LindsAndrus@hotmail.com>
2016-03-29 20:58:26 -0700

    updated Models to account for many to many relationship

--- server/models/place.js
+++ server/models/place.js
@@ -1,10 +1,20 @@
 console.log("made it to the place model")
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var PlacesSchema = new mongoose.Schema({
   city: String,
-  country: String
+  country: String,
+  latitude: Number,
+  longitude: Number,
+  _traveler: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Travelers'
+    }],
+    default: []
+  }
 });
 
-mongoose.model('places', PlacesSchema);
+var Places = mongoose.model('places', PlacesSchema);
--- server/models/traveler.js
+++ server/models/traveler.js
@@ -1,6 +1,7 @@
 console.log("made it to the traveler model")
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var TravelersSchema = new mongoose.Schema(
   {
@@ -10,16 +11,30 @@ var TravelersSchema = new mongoose.Schema(
     },
     username: {
       type: String,
-      required: true
+      min_length: 4
     },
     password: {
       type: String,
-      required: true
-    }
+      min_length: 8
+    },
+    place: {
+      type: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Places',
+        checkedIn: Boolean
+      }],
+      default: []
+    },
   },
   {
-    timestamps: true
+    timestamps: {
+      createdAt:'created_at',
+      updatedAt:'updated_at'
+    }
   }
 );
+TravelersSchema.path('name').required(true, 'Name cannot be blank');
+TravelersSchema.path('username').required(true, 'Username cannot be blank');
+TravelersSchema.path('password').required(true, 'Password cannot be blank');
 
-mongoose.model('travelers', TravelersSchema);
+var Travelers = mongoose.model('travelers', TravelersSchema);
