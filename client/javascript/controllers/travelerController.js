794d9f2d3aad16a44a83d0ad20f00ed07322e762
Ethan Choi <dchoi33@yahoo.com>
2016-03-31 11:53:31 -0700

    new google api key

--- client/javascript/controllers/travelerController.js
+++ client/javascript/controllers/travelerController.js
@@ -41,9 +41,32 @@ myApp.controller('travelers', ['$scope', '$location', '$cookies', 'travelersFact
 
   //Data from autocomplete input
   $scope.searchResult = function(){
-    // console.log($scope.search.result);
     var input = document.getElementById('searchTextField');
-    console.log(input.value);
+    // console.log(input.value);
+    if(input.value){
+      //Ajax request to google server
+      var xhttp = new XMLHttpRequest();
+      xhttp.open('GET', "https://maps.googleapis.com/maps/api/place/details/json?type=" + input.value + "$key=AIzaSyCd9oNLcDRcgRqzO_0moK-NwVv6k3nugX8", true);
+
+      xhttp.send();
+
+      xhttp.addEventListener("readystatechange", processRequest, false);
+
+      xhttp.onreadystatechange = processRequest;
+
+      function processRequest(e) {
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+        // var response = JSON.parse(xhr.responseText);
+        console.log(xhttp.responseText);
+        }
+      }
+
+      // var xhttp = new XMLHttpRequest();
+      //
+      // xhttp.open("GET", "http://ipinfo.io/json/", true);
+      // xhhtp.send();
+
+    }
   };
 
 }]);
